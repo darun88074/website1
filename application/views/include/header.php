30d0a2eed2a8c14b9eebd3b384ad856ba832391c
arunkumardhayalan <111515143+arunkumardhayalan@users.noreply.github.com>
2022-12-23 22:29:47 +0530

    second

---
+++ application/views/include/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>website1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <meta name="msapplication-tap-highlight" content="no">
+    <link href="<?php echo base_url(); ?>assets/img/fav.png" rel="shortcut icon">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-datepicker3.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/jquery.mobile-1.4.5.min.css" media="screen">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+    <!-- <link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" rel="stylesheet" /> -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/jquery.dataTables.css">
+    <link href="<?php echo base_url(); ?>assets/css/buttons.dataTables.min.css" rel="stylesheet" />
+    <link href="<?php echo base_url(); ?>assets/fontawesome/css/all.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/tagify.css" rel="stylesheet" />
+    <link href="<?php echo base_url(); ?>assets/css/themestyle.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/jquery.multiselect.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/multiple-select.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/filter_multi_select.css">
+    <link href="<?php echo base_url(); ?>assets/css/email.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/fullcalendar.css" rel="stylesheet">
+
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com"> -->
+    <!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> -->
+
+    <!-- <link href="<?php echo base_url(); ?>assets/css/css2.css" rel="stylesheet"> -->
+    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- start preloader -->
+    <div class="preloader_div">
+        <div class="preloader">
+            <div class="sub_loader"></div>
+        </div>
+        <div class="text-center preloader_center">Please Wait...</div>
+    </div>
+    <!-- end preloader -->
